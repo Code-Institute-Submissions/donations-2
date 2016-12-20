7db141608dac1c3fd40b78661e7aafafd83a2683
Seamus Woods <seamuswoods3@gmail.com>
2016-12-20 07:14:30 +0000

    hope

--- donations.py
+++ donations.py
@@ -11,12 +11,13 @@ app = Flask(__name__)
 MONGODB_URI = os.getenv('MONGODB_URI')
 MONGODB_HOST = 'ds139187'
 MONGODB_PORT = 39187
-DBS_NAME = 'MONGO_DB_NAME'
+DBS_NAME = os.getenv('MONGO_DB_NAME', 'donorsUSA')
+COLLECTION_NAME = os.getenv('MONGO_COLLECTION_NAME', 'projects')
 
 # DBS_NAME = 'donorsUSA'
 # COLLECTION_NAME = 'projects'
 #DBS_NAME = os.getenv('MONGO_DB_NAME','donorsUSA')
-COLLECTION_NAME = 'projects'
+# COLLECTION_NAME = 'projects'
 FIELDS = {'funding_status': True, 'school_state': True, 'resource_type': True, 'poverty_level': True,
           'date_posted': True, 'total_donations': True,'primary_focus_area': True,'primary_focus_subject': True,'grade_level': True, '_id': False}
 
@@ -39,13 +40,14 @@ def register():
 def donor_projects():
     # connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
     connection = MongoClient(MONGODB_URI)
+
     collection = connection[DBS_NAME][COLLECTION_NAME]
-    Projects = collection.find(projection=FIELDS, limit=20000)
+    projects = collection.find(projection=FIELDS, limit=20000)
 
 
 
     json_projects = []
-    for project in Projects:
+    for project in projects:
         json_projects.append(project)
     json_projects = json.dumps(json_projects)
     connection.close()
